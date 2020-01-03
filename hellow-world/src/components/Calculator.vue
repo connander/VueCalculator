9870e556107b0d66b1289c169da745a60393f3cb
connander <connorpstark@gmail.com>
2020-01-03 14:26:22 -0600

    started work on adding functionality to buttons

--- hellow-world/src/components/Calculator.vue
+++ hellow-world/src/components/Calculator.vue
@@ -1,17 +1,40 @@
 <template>
   <div class ="calculator">
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
-    <div>Hello</div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">*</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class ="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      current: '1',
+    }
+  },
+  methods: {
+    clear() {
+      this.current= '';
+    }
+  }
 
 }
 </script>
@@ -19,8 +42,29 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .calculator{
+  font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+}
+
+.zero {
+  grid-column: 1/3;
+}
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
